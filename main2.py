f0dffc8b96e4f5894dfb94d1c20a9cff751374b0
sergannn <su.vasiliev@gmail.com>
2023-10-05 02:38:23 +0300

    Update main2.py
    phone_code_hash

--- main2.py
+++ main2.py
@@ -10,9 +10,9 @@ async def start_auth(api_id: int, api_hash: str, phone: str):
         client = TelegramClient(phone, api_id, api_hash)
         await client.connect()
         if not await client.is_user_authorized():
-            await client.send_code_request(phone)
-            phone_code_hash = client.send_code_request(phone)
-            return {"message": "Введите код от TG", "hash":phone_code_hash}
+            result = await client.send_code_request(phone, force_sms=True)
+            phone_code_hash = result.phone_code_hash
+            return {"message": "Введите код от TG", "phone_code_hash": phone_code_hash}
         return {"message": "Авторизован"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
