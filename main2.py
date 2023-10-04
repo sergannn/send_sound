b245f016f4b74ce12e057a660148629dd55a38e9
sergannn <su.vasiliev@gmail.com>
2023-10-05 02:41:29 +0300

    Update main2.py
    cash

--- main2.py
+++ main2.py
@@ -18,11 +18,11 @@ async def start_auth(api_id: int, api_hash: str, phone: str):
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/verify_code")
-async def verify_code(api_id: int, api_hash: str, phone: str, code: str):
+async def verify_code(api_id: int, api_hash: str, phone: str, code: str, phone_code_hash: str):
     try:
         client = TelegramClient(phone, api_id, api_hash)
         await client.connect()
-        await client.sign_in(phone, code)
+        await client.sign_in(phone, code, phone_code_hash=phone_code_hash)
         return {"message": "Авторизован"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
